b668cc0f53276d292b7b8a91a07f09ddbcf32fdf
thisisamondi <67878188+thisisamondi@users.noreply.github.com>
2021-03-03 15:44:22 +0100

    Create ModalProfile.vue

---
+++ src/components/ModalProfile.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="container">
+        <div class="profilePicture">
+            <!-- profile picture -->
+        </div>
+
+        <div class="nameEmail">
+            <h1>{{}}</h1>
+            <p>{{}}</p>
+        </div>
+
+        <div class="orderContainer">
+            <h1>Orderhistorik</h1>
+        </div>
+        
+    </div>
+</template>
+
+<script>
+    export default {
+        
+
+    }
+</script>
+
+<style scoped>
+
+.container {
+    width: 375px;
+    height: 667px;
+}
+
+</style>
