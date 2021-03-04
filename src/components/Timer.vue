5111316542c8a14776361a536bd1742dad988231
leilaiths <leilaamouri@iths.se>
2021-03-04 12:01:21 +0100

    hej

---
+++ src/components/Timer.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <b>{{ timerCount }} </b>minuter
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      timerCount: 13,
+    };
+  },
+
+  watch: {
+    timerCount: {
+      handler(value) {
+        if (value > 0) {
+          setTimeout(() => {
+            this.timerCount--;
+          }, 60000);
+        }
+      },
+      immediate: true, // This ensures the watcher is triggered upon creation
+    },
+  },
+};
+</script>
+
+<style scoped></style>
